f7a629265bcdd8faf4e7c7de871fc48fd8fe4143
Eddie <daiximing199078@gmail.com>
2016-04-14 18:44:26 +0800

    fix nodemon watching by extension

--- src/server/serve.js
+++ src/server/serve.js
@@ -2,12 +2,13 @@ import app from './app'
 import http from 'http'
 
 var port = process.env.PORT || 3000
+var devPort = Number(port) + 1
 
 if (process.env.NODE_ENV !== 'production') {
   var proxy = require('proxy-middleware')
   var config = require('../../webpack.config')
   var url = require('url')
-  app.use(config.output.publicPath, proxy(url.parse(`http://localhost:${port + 1}/js/`)))
+  app.use(config.output.publicPath, proxy(url.parse(`http://localhost:${devPort}/js/`)))
 }
 
 var server = http.createServer(app)
