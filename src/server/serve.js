3919ac5bc98deb3e50e70591727b5c276d2e4b89
Eddie <daiximing199078@gmail.com>
2016-04-18 16:20:23 +0800

    update

--- src/server/serve.js
+++ src/server/serve.js
@@ -1,14 +1,18 @@
 import app from './app'
 import http from 'http'
 
+import { DEV_MODE, output } from '../../webpack.config'
+
 var port = process.env.PORT || 3000
 var devPort = Number(port) + 1
 
-if (process.env.NODE_ENV !== 'production') {
+if (DEV_MODE) {
   var proxy = require('proxy-middleware')
-  var config = require('../../webpack.config')
   var url = require('url')
-  app.use(config.output.publicPath, proxy(url.parse(`http://localhost:${devPort}/js/`)))
+  app.use(output.publicPath, proxy(url.parse(url.resolve(`http://localhost:${devPort}/`, output.publicPath))))
+} else {
+  var express = require('express')
+  app.use(output.publicPath, express.static(output.path))
 }
 
 var server = http.createServer(app)
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
-var webpack = require('webpack')
 var path = require('path')
+var DEV_MODE = process.env.NODE_ENV !== 'production'
+var plugins = []
+// var cssLoaders = 'css-loader!postcss-loader'
+
+if (DEV_MODE) {
+  var webpack = require('webpack')
+  plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
 
 module.exports = {
+  DEV_MODE: DEV_MODE,
   entry: {
     app: [
       './src/client/app.js'
     ]
   },
   output: {
-    path: path.join(__dirname, 'dist/client/js/'), // deploy path
+    path: path.join(__dirname, 'dist/client/public/'), // deploy path
     filename: '[name].js',
-    publicPath: '/js/' // dev path
+    publicPath: '/public/' // dev path
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   module: {
     loaders: [{
       test: /\.jsx?$/,
+      exclude: /node_modules/,
       loaders: ['babel']
     }]
   }
